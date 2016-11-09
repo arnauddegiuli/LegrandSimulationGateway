f63cd05fb8cefba44d0ca9b3cadf281281731600
arnaud <arnaud.degiuli@free.fr>
2016-11-09 19:57:52 +0100

    update pom to make travis-ci works

--- heating/src/main/java/com/homesnap/simulationServer/controllermodules/heating/HeatingCommand.java
+++ heating/src/main/java/com/homesnap/simulationServer/controllermodules/heating/HeatingCommand.java
@@ -1,10 +1,10 @@
-package com.homesnap.simulationServer.controllermodules.heating;
-
+//package com.homesnap.simulationServer.controllermodules.heating;
+//
 /*
  * #%L
  * HomeSnap Legrand Simulation Gateway HeatingModule
  * %%
- * Copyright (C) 2011 - 2014 A. de Giuli
+ * Copyright (C) 2011 - 2016 A. de Giuli
  * %%
  * This file is part of MyDomo done by A. de Giuli (arnaud.degiuli(at)free.fr).
  * 
@@ -22,60 +22,61 @@ package com.homesnap.simulationServer.controllermodules.heating;
  *     along with MyDomo.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
-
-
-import java.text.MessageFormat;
-import java.util.List;
-
-import com.homesnap.engine.connector.openwebnet.OpenWebNetConstant;
-import com.homesnap.engine.connector.openwebnet.convert.OpenWebNetConverterRegistry;
-import com.homesnap.engine.connector.openwebnet.dimension.DimensionValue;
-import com.homesnap.simulationServer.ControllerStateManagement;
-
-public class HeatingCommand {
-	
-	// OSGi Shell Function	
-	static final String[] functions = {
-		"desiredTemperature", "currentTemperature"};
-
-	/**
-	 * Change the temperature of the heating system
-	 * @param day
-	 * @param month
-	 * @param year
-	 * @return
-	 */
-	public String desiredTemperature(double temperature, String address) {
-		DesiredTemperature dt = new DesiredTemperature();
-		dt.setDesiredTemperature(temperature);
-		dt.setMode(1);
-		return ControllerStateManagement.executeCommand(MessageFormat.format(OpenWebNetConstant.DIMENSION_COMMAND, new Object[] {OpenWebNetConverterRegistry.WHO_HEATING_ADJUSTMENT, address, HeatingZoneDimension.SET_TEMPERATURE, formatDimension(dt.getValueList())}));
-	}
-	
-	/**
-	 * Simulate the current temperature of a zone
-	 * @param day
-	 * @param month
-	 * @param year
-	 * @return
-	 */
-	public String currentTemperature(double temperature, String address) {
-		MeasureTemperature dt = new MeasureTemperature();
-		dt.setMeasuredTemperature(temperature);
-		return ControllerStateManagement.executeCommand(MessageFormat.format(OpenWebNetConstant.DIMENSION_COMMAND, new Object[] {OpenWebNetConverterRegistry.WHO_HEATING_ADJUSTMENT, address, HeatingZoneDimension.MEASURE_TEMPERATURE.getCode(), formatDimension(dt.getValueList())}));
-	}
-	
-	private String formatDimension(List<DimensionValue> valueList) {
-		StringBuilder sb = new StringBuilder();
-		for (DimensionValue value : valueList) {
-			sb.append(value.getValue());
-			sb.append(OpenWebNetConstant.DIMENSION_SEPARATOR);
-		}
-		
-		if (sb.length() > 0) {
-			sb.setLength(sb.length()-1);
-		}
-		
-		return sb.toString();
-	}
-}
+//
+//
+//import java.text.MessageFormat;
+//import java.util.List;
+//
+//import com.homesnap.engine.connector.openwebnet.OpenWebNetConstant;
+//import com.homesnap.engine.connector.openwebnet.convert.OpenWebNetConverterRegistry;
+//import com.homesnap.engine.connector.openwebnet.dimension.DimensionValue;
+//import com.homesnap.engine.connector.openwebnet.heating.dimension.MeasureTemperature;
+//import com.homesnap.simulationServer.ControllerStateManagement;
+//
+//public class HeatingCommand {
+//	
+//	// OSGi Shell Function	
+//	static final String[] functions = {
+//		"desiredTemperature", "currentTemperature"};
+//
+//	/**
+//	 * Change the temperature of the heating system
+//	 * @param day
+//	 * @param month
+//	 * @param year
+//	 * @return
+//	 */
+//	public String desiredTemperature(double temperature, String address) {
+//		DesiredTemperature dt = new DesiredTemperature();
+//		dt.setDesiredTemperature(temperature);
+//		dt.setMode(1);
+//		return ControllerStateManagement.executeCommand(MessageFormat.format(OpenWebNetConstant.DIMENSION_COMMAND, new Object[] {OpenWebNetConverterRegistry.WHO_HEATING_ADJUSTMENT, address, HeatingZoneDimension.SET_TEMPERATURE, formatDimension(dt.getValueList())}));
+//	}
+//	
+//	/**
+//	 * Simulate the current temperature of a zone
+//	 * @param day
+//	 * @param month
+//	 * @param year
+//	 * @return
+//	 */
+//	public String currentTemperature(double temperature, String address) {
+//		MeasureTemperature dt = new MeasureTemperature();
+//		dt.setMeasuredTemperature(temperature);
+//		return ControllerStateManagement.executeCommand(MessageFormat.format(OpenWebNetConstant.DIMENSION_COMMAND, new Object[] {OpenWebNetConverterRegistry.WHO_HEATING_ADJUSTMENT, address, HeatingZoneDimension.MEASURE_TEMPERATURE.getCode(), formatDimension(dt.getValueList())}));
+//	}
+//	
+//	private String formatDimension(List<DimensionValue> valueList) {
+//		StringBuilder sb = new StringBuilder();
+//		for (DimensionValue value : valueList) {
+//			sb.append(value.getValue());
+//			sb.append(OpenWebNetConstant.DIMENSION_SEPARATOR);
+//		}
+//		
+//		if (sb.length() > 0) {
+//			sb.setLength(sb.length()-1);
+//		}
+//		
+//		return sb.toString();
+//	}
+//}
--- heating/src/main/java/com/homesnap/simulationServer/controllermodules/heating/HeatingModuleActivator.java
+++ heating/src/main/java/com/homesnap/simulationServer/controllermodules/heating/HeatingModuleActivator.java
@@ -4,7 +4,7 @@ package com.homesnap.simulationServer.controllermodules.heating;
  * #%L
  * HomeSnap Legrand Simulation Gateway HeatingModule
  * %%
- * Copyright (C) 2011 - 2014 A. de Giuli
+ * Copyright (C) 2011 - 2016 A. de Giuli
  * %%
  * This file is part of MyDomo done by A. de Giuli (arnaud.degiuli(at)free.fr).
  * 
@@ -21,7 +21,7 @@ package com.homesnap.simulationServer.controllermodules.heating;
  *     You should have received a copy of the GNU General Public License
  *     along with MyDomo.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
- */
+*/
 
 
 import java.util.Dictionary;
@@ -34,24 +34,24 @@ import com.homesnap.simulationServer.ControllerStateManagement;
 
 public class HeatingModuleActivator implements BundleActivator {
 
-	private HeatingSimulator heatingModule = new HeatingSimulator();
+//	private HeatingSimulator heatingModule = new HeatingSimulator();
 	
 	@Override
 	public void start(BundleContext context) throws Exception {
 		
-		Dictionary<String, Object> dict = new Hashtable<String, Object>();
-		dict.put("osgi.command.scope", "heating");
-		dict.put("osgi.command.function", HeatingCommand.functions);
-
-		
-		context.registerService(HeatingCommand.class.getName(), new HeatingCommand(), dict);
-		
-		ControllerStateManagement.registerControllerDimensionCommand(heatingModule);
+//		Dictionary<String, Object> dict = new Hashtable<String, Object>();
+//		dict.put("osgi.command.scope", "heating");
+//		dict.put("osgi.command.function", HeatingCommand.functions);
+//
+//		
+//		context.registerService(HeatingCommand.class.getName(), new HeatingCommand(), dict);
+//		
+//		ControllerStateManagement.registerControllerDimensionCommand(heatingModule);
 	}
 
 	@Override
 	public void stop(BundleContext arg0) throws Exception {
-		ControllerStateManagement.registerControllerDimensionCommand(heatingModule);
+//		ControllerStateManagement.registerControllerDimensionCommand(heatingModule);
 	}
 	
 	
